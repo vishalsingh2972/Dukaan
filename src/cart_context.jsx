446b1772213315722cc7385d64536c843d6e37c7
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-09 16:47:25 +0530

    Update cart_context.jsx

--- src/cart_context.jsx
+++ src/cart_context.jsx
@@ -18,6 +18,45 @@ export function CartProvider({children}) {
 
   const [cartProducts, setCartProducts] = useState([]);
 
+  // in our cart we are going to store 2 things: id of the item, quantity of the item {id:1, quantity:3}
+  const getProductQuantity = (id) => {
+    const quantity = cartProducts.find((product) => product.id === id)?.quantity;
+
+    if (quantity === undefined){
+      return 0;
+    }
+
+    return quantity;
+  }
+
+  // product is in cart case ===> {id:1, quantity:3} ,for id = 1 => {id:1, quantity:4}
+  // product is not in cart case ===> {id:1, quantity:3} ,for id = 2 => [{id:1, quantity:3}, {id:2, quantity:1}] 
+  const addOneItemToCart = (id) => {
+    const quantity = getProductQuantity(id);
+
+    if (quantity === 0){ //product is not in cart case
+      setCartProducts(
+        [
+          ...cartProducts, //keep existing cartProducts in front of the array
+          {
+            id: id,
+            quantity: 1
+          }
+        ]
+      );
+    }
+    else{ //product is in cart case
+      setCartProducts(
+        cartProducts.map(
+          (product) => {
+            product.id === id ? { ...product, quantity: product.quantity + 1} : product;
+          }
+        )
+      )
+    }
+  }
+
+
   const contextValue = {
     items: cartProducts,
     getProductQuantity,
@@ -30,5 +69,3 @@ export function CartProvider({children}) {
     <cart_context.Provider value={contextValue}>
       {children}
     </cart_context.Provider>
-  )
-}
