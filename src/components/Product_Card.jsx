619aec5fde1eb5b4d49f467e256de4f65dd73eb9
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-10 14:28:19 +0530

    Update Product_Card.jsx

--- src/components/Product_Card.jsx
+++ src/components/Product_Card.jsx
@@ -1,16 +1,24 @@
 import { Card, Button, Form, Row, Col } from "react-bootstrap"
+import { cart_context } from "../cart_context";
+import { useContext } from "react";
 
 const Product_Card = (props) => { //props.product is the product we are selling
   const product = props.product;
   // console.log(props)
   // console.log(product)
 
+  //accessing cart object from cart_context
+  const cart = useContext(cart_context);
+  // console.log(`bittu ${cart.getTotalCost}`);
+  const productQuantity = cart.getProductQuantity(product.id);
+  console.log(cart.items);
+  
   return (
     <Card>
       <Card.Body>
         <Card.Title>{product.title}</Card.Title>
         <Card.Text>₹ {product.price}</Card.Text>
-        <Button variant="primary">Add to Cart</Button>
+        <Button variant="primary" onClick={() => cart.addOneItemToCart(product.id)}>Add to Cart</Button>
       </Card.Body>
     </Card>
   )
