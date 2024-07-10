aed0d074bae95b2965e6f69fbf1f2b2e4ff13493
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-10 15:07:25 +0530

    Update Navbar_Component.jsx

--- src/components/Navbar_Component.jsx
+++ src/components/Navbar_Component.jsx
@@ -1,12 +1,17 @@
 import { Button, Container, Navbar, Modal } from "react-bootstrap"
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { cart_context } from "../cart_context";
 
 const Navbar_Component = () => {
 
   const [show, setShow] = useState(false);
+  const cart = useContext(cart_context);
 
   const handleClose_funtion = () => setShow(false);
   const handleShow_function = () => setShow(true);
+
+  //function for count of total products added
+  const productCount = cart.items.reduce((sum, product /*(item)*/ ) => sum + product.quantity, 0); //the 0 at the end is the initial value for the sum accumulator in the reduce function. i.e telling start with sum = 0
 
   return (
     <>
@@ -14,7 +19,7 @@ const Navbar_Component = () => {
         <Navbar.Brand href="/">dukaan</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Button onClick={handleShow_function}>Cart 0 Items</Button>
+          <Button onClick={handleShow_function}>Cart ({productCount} Items)</Button>
         </Navbar.Collapse>
       </Navbar>
       <Modal show={show} onHide={handleClose_funtion}>
@@ -22,7 +27,11 @@ const Navbar_Component = () => {
           <Modal.Title>Shopping Cart</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h1>This is the modal body</h1>
+          {productCount > 0 ? 
+            <h1>The cart has {productCount} item(s)</h1>
+          :
+            <h1>The cart is empty</h1>
+          }
         </Modal.Body>
       </Modal>
     </>
